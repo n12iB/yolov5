a67eb891ead5e6724f577f274dcb15c6c9b56756
n12iB <106806889+n12iB@users.noreply.github.com>
2022-11-01 13:01:54 +0100

    changed aug

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -418,7 +418,6 @@ class LoadImagesAndLabels(Dataset):
         self.image_weights = image_weights
         self.rect = False if image_weights else rect
         self.mosaic = self.augment and not self.rect  # load 4 images at a time into a mosaic (only during training)
-        self.mosaic = False
         self.mosaic_border = [-img_size // 2, -img_size // 2]
         self.stride = stride
         self.path = path
